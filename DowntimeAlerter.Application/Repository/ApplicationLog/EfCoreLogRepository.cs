de77e0c22566eb2d9a793e24b4d00bebef70e7c1
Pınar BÜLBÜL <pinar.bulbul@roketsan.com.tr>
2021-09-26 23:31:09 +0300

    Log folder is changed as ApplicationLog

---
+++ DowntimeAlerter.Application/Repository/ApplicationLog/EfCoreLogRepository.cs
@@ -0,0 +1,27 @@
+﻿using DowntimeAlerter.Domain.Entities;
+using DowntimeAlerter.EntityFrameworkCore.LogDb;
+using DowntimeAlerter.EntityFrameworkCore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DowntimeAlerter.Application.Repository
+{
+    public class EfCoreLogRepository : EfCoreRepository<Log, LogDbContext>, ILogRepository
+    {
+        private readonly LogDbContext _context;
+        public EfCoreLogRepository(LogDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public IList<Log> GetLogsForWeek()
+        {
+            return _context.Log.Where(x => x.TimeStamp > DateTime.Today.AddDays(-7))
+                .OrderByDescending(x => x.TimeStamp)
+                .ToList();
+        }
+    }
+}
---
+++ DowntimeAlerter.Application/Repository/ApplicationLog/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿using DowntimeAlerter.Domain.Entities;
+using DowntimeAlerter.EntityFrameworkCore.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DowntimeAlerter.Application.Repository
+{
+    public interface ILogRepository : IRepository<Log>
+    {
+        IList<Log> GetLogsForWeek();
+    }
+}
